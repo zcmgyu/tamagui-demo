503aca6024f0ca7027e0ac40f00f1bfda5717a12
zcmgyu <zcmgyu@gmail.com>
2023-09-29 20:33:50 +0900

    issue: could not set color tokens

--- packages/app/provider/index.tsx
+++ packages/app/provider/index.tsx
@@ -10,7 +10,7 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
     <TamaguiProvider
       config={config}
       disableInjectCSS
-      defaultTheme={scheme === 'dark' ? 'dark' : 'light'}
+      defaultTheme={scheme === 'dark' ? 'dark_green' : 'light_green'}
       {...rest}
     >
       <ToastProvider
